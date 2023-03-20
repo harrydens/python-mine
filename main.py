e34c9489118cdac2db6aa48634f5ea851443c5c4
harrydens <hsrry7272@gmail.com>
2023-03-20 17:17:47 +0900

    push

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
+
 from PIL import ImageTk, Image, ImageFilter
+from io import BytesIO
 import tkinter as tk
+import requests
+
 
 size = int(input("크기:"))
 board = [[0 for i in range(size)] for i in range(size)]
@@ -8,8 +12,9 @@ window = tk.Tk()
 canvas = tk.Canvas(window, width =size*20,height=size*20)
 canvas.pack()
 
-test = "f_block.png"
-i_photo = Image.open(test)
+url = "https://raw.githubusercontent.com/harrydens/python-mine/main/image/f_blcok.png" ## 만약 "https://github.com/harrydens/python-mine/blob/main/image/f_blcok.png"이 코드에서 이미지를 로드하는 URL이라면, 이 URL은 정확한 경로가 아닙니다. GitHub에서 이미지 파일에 직접 링크를 만드는 대신, GitHub는 링크를 로드하면 GitHub 페이지를 반환하고 페이지에 이미지가 포함된 태그가 있는 경우 이미지를 반환합니다.따라서 코드에서 이미지를 로드하려면, 해당 이미지를 호스팅하는 Raw URL을 사용해야 합니다. 이 경우, URL은 "https://raw.githubusercontent.com/harrydens/python-mine/main/image/f_blcok.png"와 같아야 합니다.  ##기억하자!!
+respond = requests.get(url)
+i_photo = Image.open(BytesIO(respond.content))
 re_photo = i_photo.resize((20,20))
 photo = ImageTk.PhotoImage(re_photo)
 
@@ -32,7 +37,7 @@ def rep():
     for i in range(0,size):
         for j in range(0,size):
             button = canvas.create_image(20*j,20*i,anchor=tk.NW ,image=board[i][j])
-            canvas.tag_bind(button, "", lambda event: test(i,j))
+            canvas.tag_bind(button, "", lambda event: print("a"))
 
 def p_o(x,y,size_x,size_y,img):
     resized_img = img.resize((size_x,size_y), Image.LANCZOS)
